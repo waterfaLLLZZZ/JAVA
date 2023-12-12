e81b5ab4d7437e8e297198fc2183d5d6f849fc1b
Ivan <91288796+waterfaLLLZZZ@users.noreply.github.com>
2023-12-12 23:54:26 +0300

    Delete Task5-6/Task5 directory

--- Task5-6/Task5/Observable.java
+++
@@ -1,7 +0,0 @@
-public interface Observable {
-    void subscribe(Observer observer);
-    void unsubscribe(Observer observer);
-    void notifyObservers(String results);
-    void result(String result);
-
-}
